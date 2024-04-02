e7b79d10c1ac2549fac707fd70dc961857cd9316
JuneMbuvi <mbuvijune@gmail.com>
2024-04-02 17:37:27 +0000

    Adding CSS & Images

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'production',
+	entry: path.resolve(__dirname, './js/dashboard_main.js'),
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'public')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			},
+			{
+				test: /\.(png|jpg|gif|svg)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {}
+					}
+				]
+			},
+		]
+	},
+};
