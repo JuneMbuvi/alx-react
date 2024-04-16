b281dec670837997f0aeec9c3ef1bee2e267bacf
JuneMbuvi <mbuvijune@gmail.com>
2024-04-16 13:32:23 +0000

    Animations

---
+++ 0x04-React_inline_styling/task_4/dashboard/src/Header/Header.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import { shallow } from 'enzyme';
+
+StyleSheetTestUtils.suppressStyleInjection();
+
+describe('<Header />', () => {
+	it('renders  without crashing', () => {
+		const wrapper = shallow(<Header />);
+		expect(wrapper.exists()).toBe(true);
+	})
+	it('tests that the components render img and h1 tags', () => {
+                const wrapper = shallow(<Header />);
+                expect(wrapper.exists('img')).toBe(true);
+		expect(wrapper.exists('h1')).toBe(true);
+        })
+})
