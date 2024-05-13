4446a1a1a0f686c32b7fa5bd03daebf9828e756e
June <mbuvijune@gmail.com>
2024-05-13 15:01:44 +0000

    Combine store: modify the application

--- 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
@@ -5,10 +5,13 @@ import { createStore } from 'redux';
 import { uiReducer } from './reducers/uiReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import rootReducer from "./reducers/rootReducer";
 
 
 const reduxExtension = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || composeWithDevTools;
-const store = createStore(uiReducer, Map(initialState), reduxExtension(applyMiddleware(thunk)));
+const store = createStore(rootReducer, reduxExtension && reduxExtension());
+
+
 ReactDOM.render(
 	<React.StrictMode>
 	<Provider store={store}>
